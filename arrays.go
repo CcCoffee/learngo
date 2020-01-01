1424fb8c7b114b87ee1a251dd3e03aa148132892
Kevin K H Zheng <172853425@qq.com>
2020-01-02 00:38:57 +0800

    3-1 数组的遍历

---
+++ arrays.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func max(numbers [4]int) (int, int) {
+	maxi := -1
+	maxValue := 01
+	for i, v := range numbers {
+		if v > maxValue {
+			maxi, maxValue = i, v
+		}
+	}
+	return maxi, maxValue
+}
+
+func main() {
+	//数量写在类型前面
+	var arr1 [5]int
+	arr2 := [3]int{1, 3, 4}       //:=需要给初始值
+	arr3 := [...]int{2, 3, 4, 5}  //让编译器去数总数
+	fmt.Println(arr1, arr2, arr3) //[0 0 0 0 0] [1 3 4] [2 3 4 5]
+
+	//二维数组
+	var grid [4][5]int
+	fmt.Println(grid) //[[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]]
+
+	//遍历数组方式一
+	for i := 0; i < len(arr3); i++ {
+		fmt.Println(arr3[i])
+	}
+
+	//遍历数组方式二
+	for i := range arr3 {
+		fmt.Println(arr3[i])
+	}
+
+	//遍历数组方式三
+	for i, v := range arr3 {
+		fmt.Println(i, v)
+	}
+
+	//遍历数组方式四, 下划线省略变量
+	for _, v := range arr3 {
+		fmt.Println(v)
+	}
+
+	maxi, maxValue := max(arr3)
+	fmt.Println(maxi, maxValue)
+
+}
