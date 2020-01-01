0d43c89c44af0f80b8f9e53cf22a7f5fb16004e3
Kevin K H Zheng <172853425@qq.com>
2020-01-01 23:24:07 +0800

    2-4 switch

--- branch.go
+++ branch.go
@@ -9,6 +9,30 @@ func main() {
 	const filename = "abc.txt"
 	//test1(filename)
 	test2(filename)
+	fmt.Println(grade(30))
+	fmt.Println(grade(111))
+
+}
+
+/**
+switch后面可以没有表达式，在case里面加入条件就可以
+*/
+func grade(score int) string {
+	result := ""
+	switch {
+	case score < 60:
+		result = "F"
+	case score < 80:
+		result = "C"
+	case score < 90:
+		result = "B"
+	case score < 100:
+		result = "A"
+	default:
+		//使用panic会中断程序执行，报错
+		panic(fmt.Sprintf("Wrong score: %d\n", score))
+	}
+	return result
 }
 
 /**
