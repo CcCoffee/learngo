a7505b08245f659783e387fa2957711253149d36
Kevin K H Zheng <172853425@qq.com>
2020-01-03 00:19:17 +0800

    3-6 rune类型与unicode和utf-8的关系

--- strings.go
+++ strings.go
@@ -11,4 +11,20 @@ func main() {
 	//	10XXXXXX -> 1000 1000 剩余0110 XXXX XXXX XXXX
 	//	1110XXXX -> 1110 0110
 	//=> 1110 0110 1000 1000 1001 0001 这就是'我'的UTF-8编码，占用了三个字节
+
+	s := "Aa我是Kevin"
+	fmt.Println(len(s)) //13,这里的s变量数据内容其实是经utf-8编码过的
+	// len函数可以打印出字符串占用的字节数，utf-8编码的unicode中每个中文通常占三个字节byte，少数占用4个字节
+	for _, ch := range []byte(s) {
+		fmt.Printf("%X ", ch) //41 61 E6 88 91 E6 98 AF 4B 65 76 69 6E
+	}
+	fmt.Println()
+
+	//rune 等同于int32，即4个字节长度,常用来处理unicode编号或utf-8字符
+	for i, ch := range s { //range把string进行了utf-8的解码，解出来每个字符又进行了转译为unicode编号，转完放在了rune这个四字节类型的变量ch里面
+		// 注意： unicode编号是世界上所有字符的唯一编号，它仅占2个字节，但是转化为能够存储的utf-8编码格式时占用的字符数却不一定是2个字符
+		// utf-8有可能占用1、2、3或4个字符，如ascii编码的字符仅占用1个字符。而中文大部分需要占用3个字符。具体编码过程参见上面的url链接文章
+		fmt.Printf("(%d %X) ", i, ch) //(0 41) (1 61) (2 6211) (5 662F) (8 4B) (9 65) (10 76) (11 69) (12 6E)
+	}
+	fmt.Println()
 }
