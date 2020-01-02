b6c4d87842d2c7098e3317c90760e080a70d9ee6
Kevin K H Zheng <172853425@qq.com>
2020-01-02 21:10:13 +0800

    3-4 map判断值存在否

--- map.go
+++ map.go
@@ -17,10 +17,29 @@ func main() {
 	fmt.Println(m3)       //map[]
 
 	fmt.Println("map的遍历")
+	//map是hashmap，是无序的
 	for k, v := range m {
 		fmt.Printf("k:v = %s:%s\n", k, v)
 	}
 	for k := range m {
 		fmt.Printf("k = %s\n", k)
 	}
+
+	name := m["name"]
+	fmt.Println(name)
+
+	//获取map中不存在的key，拿到的是zero value，即空字符串
+	name1 := m["name1"]
+	fmt.Println(name1)
+
+	//判断key在不在
+	name2, ok := m["name2"]
+	fmt.Println(name2, ok) // false
+
+	if name3, ok := m["name3"]; ok {
+		fmt.Println(name3, ok) // name3不存在，所以不会被执行
+	} else {
+		fmt.Println("name3不存在")
+	}
+
 }
