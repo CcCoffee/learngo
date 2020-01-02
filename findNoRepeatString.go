dc9a9f132197d8ed9c8ab41266a5eb10aac32366
Kevin K H Zheng <172853425@qq.com>
2020-01-02 22:10:56 +0800

    3-5 利用map算出字符串中最长不重复字符子串的长度

---
+++ findNoRepeatString.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+//算法：寻找最长的不含有重复字符的子串 abcabcbb -> abc， bbbbb->b, pwwkew -> wke
+//对于每一个字母x
+//* lastOccurred[x]不存在，或者 < start ->无需操作
+//* lastOccurred[x] >= start -> 更新start
+//* 更新lastOccurred[x]，更新maxLength
+
+//主要时利用了map进行查字段
+func findMaxLengthFromString(content string) int {
+	//()(start)()()(*)()()
+	//-->不断向前移动
+	//()(start)()()()(*)()
+	//for循环遍历每个字符，go没有char
+
+	//start指针的移动由lastOccurred[x]是否存在决定
+	start := 0
+	lastOccurred := make(map[byte]int)
+	maxLength := 0
+
+	for i, ch := range []byte(content) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength { //maxLength只在出现更长的不重复字符子串时才进行更新
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
+func main() {
+	fmt.Println(findMaxLengthFromString("abbcdee"))
+	fmt.Println(findMaxLengthFromString("aa"))
+	fmt.Println(findMaxLengthFromString(""))
+}
