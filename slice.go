1bff56d74747636a33ebd571a0f0a0201c86df22
Kevin K H Zheng <172853425@qq.com>
2020-01-02 20:29:17 +0800

    3-3 slice的添加元素操作

--- slice.go
+++ slice.go
@@ -34,6 +34,19 @@ func main() {
 	a2 := a1[3:5]                                                       //[5 6]，slice可以向后拓展，不可向前拓展。
 	fmt.Printf("a1=%v, len(a1)=%d, cap(a1)=%d\n", a1, len(a1), cap(a1)) //a1=[2 3 4 5], len(a1)=4, cap(a1)=6 //cap(a1)=6,是因为有[2 3 4 5 6 7]
 	fmt.Printf("a2=%v, len(a2)=%d, cap(a2)=%d\n", a2, len(a2), cap(a2)) //a2=[5 6], len(a2)=2, cap(a2)=3 //cap(a1)=3,是因为有[5 6 7]
-	fmt.Println(a2[3:6])                                                //[5 6 7]
+	fmt.Println(a1[3:6])                                                //[5 6 7]
 	//fmt.Println(a2[3:7])//panic: runtime error: slice bounds out of range
+
+	fmt.Println("--------- 向slice添加元素 ---------")
+	fmt.Println("testArray = ", testArray) //testArray =  [0 1 2 3 4 5 6 7]
+	a3 := append(a2, 10)                   //                      a2 =            [5 6]
+	fmt.Println("a3 = ", a3)               //                     a3 =            [5 6 10]
+	a4 := append(a3, 11)
+	fmt.Println("a4 = ", a4) //                     a4 =            [5 6 10 11]
+	a5 := append(a4, 12)
+	fmt.Println("a5 = ", a5) //                     a5 =            [5 6 10 11 12]
+	//s4 and s5 no longer view testArray, system create a new longer array.
+	//添加元素时如果超越cap，系统会重新分配更大的底层数组。原来的数组如果有人使用它还可以存在，否则等待垃圾回收
+	//由于值传递的关系，必须接受append的返回值，s= append(s,10)
+	fmt.Println("testArray = ", testArray) //testArray =  [0 1 2 3 4 5 6 10]
 }
