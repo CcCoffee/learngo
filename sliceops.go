b1f5b8fd753ac7b136e22ae821b795afec7ee4e5
Kevin K H Zheng <172853425@qq.com>
2020-01-02 20:53:04 +0800

    3-3 slice的复制和删除元素

--- sliceops.go
+++ sliceops.go
@@ -33,4 +33,22 @@ func main() {
 	//16为初始长度，32为capacity
 	s3 := make([]int, 16, 32) //[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0], len=16, cap=32
 	printSlice(s3)
+
+	// 2. 复制slice
+	copy(s2, s1)
+	printSlice(s2) //[2 4 6 8 0 0 0 0 0 0 0 0 0 0 0 0], len=16, cap=16
+
+	// 3. 删除slice的元素
+	// 删除下标为3的元素8，s2[4:]...使用了go中的展开语法
+	s2 = append(s2[:3], s2[4:]...)
+	printSlice(s2) //[2 4 6 0 0 0 0 0 0 0 0 0 0 0 0], len=15, cap=16
+
+	// 4. 删除slice的头元素
+	s2 = s2[1:]
+	printSlice(s2) //[4 6 0 0 0 0 0 0 0 0 0 0 0 0], len=14, cap=15
+
+	// 5. 删除slice的尾元素
+	printSlice(s1) //[2 4 6 8], len=4, cap=4
+	s1 = s1[:(len(s1) - 1)]
+	printSlice(s1) //[2 4 6], len=3, cap=4
 }
